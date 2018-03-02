c7ace2adf625edc417019efccb461daf863b6ed9
Abdulrahman Hasan <lionroar009@gmail.com>
2018-03-02 11:48:47 +0300

    finihing my home page

--- js/javascript.js
+++ js/javascript.js
@@ -1,9 +1,17 @@
-/*global $, console */
-
-$(function (){
-  'use strict'
-  $('.header').height($(window).height())
-  $(window).resize(function(){
-    $('.header').height($(window).height())
-  })
-});
+// var $document = $(document),
+//     $element = $('.navbar'),
+//     navbarDefault = 'navbar-dark bg-dark';
+//     navbarTransparent = 'navbar-transparent';
+//     fadeInDown = 'fadeInDown';
+//
+// $document.scroll(function(){
+//   if ($document.scrollTop() >= 720){
+//     $element.addClass(navbarDefault);
+//     $element.removeClass(navbarTransparent);
+//     $element.addClass(fadeInDown);
+//   }else{
+//     $element.removeClass(navbarDefault);
+//     $element.addClass(navbarTransparent);
+//     $element.removeClass(fadeInDown);
+//   }
+// });
